bb446dfad4a0a7ad63ae7a74c2bff1cf477d22cd
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-12-18 01:23:41 +0100

    added Hummingbird text

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,31 @@
-void main(){
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hummingbird"),
+        ),
+        body: Center(
+          child: Text(
+            "Hummingbird",
+            style: TextStyle(
+              fontSize: 36.0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
