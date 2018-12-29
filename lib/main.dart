10e8b06b76fa111a7a508690c7f98542e40ba235
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-12-29 03:31:07 +0100

    added hummingbird animated text

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-//import 'dart:async';
+import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flare_flutter/flare_actor.dart';
@@ -15,11 +15,10 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
   static const String _hummingBirdString = 'Hummingbird';
 
-//  static int _currentIndex = 0;
-//
-//  String _textOne = _hummingBirdString.substring(0, _currentIndex);
-//  String _textTwo =
-//      _hummingBirdString.substring(_currentIndex, _hummingBirdString.length);
+  int _currentIndex = 0;
+
+  String _textOne;
+  String _textTwo;
 
   bool _showFlarePlaceholder = true;
 
@@ -41,12 +40,13 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
   }
 
   _buildBody() {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          _buildFlare(),
-        ],
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: _updateAnimation(),
+        ),
       ),
     );
   }
@@ -55,7 +55,7 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
     return FloatingActionButton(
       child: Icon(Icons.play_arrow),
       onPressed: () {
-        _updateAnimation();
+        _updateAnimationState();
       },
     );
   }
@@ -74,60 +74,84 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
     );
   }
 
-  _updateAnimation() {
+  _buildHummingbirdText() {
+    return Container(
+      child: Center(
+        child: RichText(
+          textAlign: TextAlign.center,
+          text: TextSpan(
+            text: _textOne,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 36.0,
+            ),
+            children: [
+              TextSpan(
+                text: _textTwo,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 36.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _updateAnimationState() {
     setState(() {
       _showFlarePlaceholder = !_showFlarePlaceholder;
     });
   }
 
-//  _buildHummingbirdText() {
-//    Timer(const Duration(milliseconds: 100), () {
-//      _getPrintableHummingBirdText();
-//    });
-//
-//    return Container(
-//      child: Center(
-//        child: RichText(
-//          textAlign: TextAlign.center,
-//          text: TextSpan(
-//            text: _textOne,
-//            style: TextStyle(
-//              color: Colors.black,
-//              fontSize: 36.0,
-//            ),
-//            children: [
-//              TextSpan(
-//                text: _textTwo,
-//                style: TextStyle(
-//                  color: Colors.white,
-//                  fontSize: 36.0,
-//                ),
-//              ),
-//            ],
-//          ),
-//        ),
-//      ),
-//    );
-//  }
+  List<Widget> _updateAnimation() {
+    if (_showFlarePlaceholder) {
+      _initEmptyHummingbirdText();
 
-//  _getPrintableHummingBirdText() {
-//    if (_currentIndex == _hummingBirdString.length + 1) {
-//      _currentIndex = 0;
-//      return;
-//    }
-//
-//    _textOne = _hummingBirdString.substring(
-//      0,
-//      _currentIndex,
-//    );
-//
-//    _textTwo = _hummingBirdString.substring(
-//      _currentIndex,
-//      _hummingBirdString.length,
-//    );
-//
-//    setState(() {
-//      _currentIndex++;
-//    });
-//  }
+      return [
+        _buildFlare(),
+      ];
+    } else {
+      Timer(const Duration(milliseconds: 125), () {
+        _updateHummingbirdText();
+      });
+
+      return [
+        _buildFlare(),
+        _buildHummingbirdText(),
+      ];
+    }
+  }
+
+  _initEmptyHummingbirdText() {
+    setState(() {
+      _textOne = "";
+      _textTwo = "";
+    });
+  }
+
+  _initHummingbirdText() {
+    setState(() {
+      _textOne = _hummingBirdString.substring(0, _currentIndex);
+      _textTwo = _hummingBirdString.substring(
+        _currentIndex,
+        _hummingBirdString.length,
+      );
+    });
+  }
+
+  _updateHummingbirdText() {
+    if (_currentIndex == _hummingBirdString.length + 1) {
+      _currentIndex = 0;
+      return;
+    }
+
+    _initHummingbirdText();
+
+    setState(() {
+      _currentIndex++;
+    });
+  }
 }
